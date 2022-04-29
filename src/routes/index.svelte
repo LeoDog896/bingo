a02bd1566a54f36fa320ae97ba3583fda54c06e9
Tristan <LeoDog896@hotmail.com>
2022-04-28 20:46:08 -0400

    rotator & stand

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,24 +1,41 @@
 <script lang="ts">
-	import * as THREE from 'three';
-	import * as SC from 'svelte-cubed';
+	import * as CANNON from "cannon-es";
+	import * as PE from "svelte-cannon";
+	import * as THREE from "three";
+	import * as SC from "svelte-cubed";
 	import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
     import { onMount } from 'svelte/internal';
     
-	let scene: THREE.Group;
+	let rotator: THREE.Group;
+	let stand: THREE.Group;
+
+	let spin = 0;
+
+	SC.onFrame(() => {
+		spin += 0.01;
+	});
+
+	let position = new CANNON.Vec3(0, 0, 0);
 
     onMount(() => {
         const loader = new GLTFLoader();
 
-        loader.load( 'roller.glb', function ( gltf ) {
+        loader.load('rotator.glb', function (gltf) {
+			rotator = gltf.scene
+		}, undefined, console.error);
 
-			scene = gltf.scene
-
-		}, undefined, function ( error ) {
-			console.error( error );
-
-		});
+		loader.load( 'stand.glb', function (gltf) {
+			stand = gltf.scene
+		}, undefined, console.error);
     })
 </script>
+
+<PE.World gravity={[0, -9.81, 0]}>
+	<PE.Body mass={1} bind:position>
+	  <PE.Sphere radius={1} />
+	  <PE.Primitive></PE.Primitive>
+	</PE.Body>
+  </PE.World>
 
 <SC.Canvas
 	antialias
@@ -39,11 +56,23 @@
 		/>
 	</SC.Group>
 
-	<SC.Primitive
-		object={scene}
-		position={[0, new THREE.Box3().setFromObject(scene).getSize(new THREE.Vector3()).y - 2, 0]}
-		rotation={[0, 0, 0]}
-	/>
+	{#if rotator}
+		<SC.Primitive
+			object={rotator}
+			position={[0, new THREE.Box3().setFromObject(rotator).getSize(new THREE.Vector3()).y - 2, 0]}
+			rotation={[spin, 0, 0]}
+		/>
+	{/if}
+
+	{#if stand}
+		<SC.Primitive
+			object={stand}
+			position={[0, new THREE.Box3().setFromObject(rotator).getSize(new THREE.Vector3()).y - 2, 0]}
+			rotation={[0, 0, 0]}
+		/>
+	{/if}
+
+	<SC.Mesh geometry={new THREE.SphereGeometry()} position={[position.x, position.y, position.z]} scale={[0.25, 0.25, 0.25]} />
 
 	<SC.PerspectiveCamera position={[1, 1, 3]} zoom={0.5} />
 	<SC.OrbitControls enableZoom={false} maxPolarAngle={Math.PI * 0.51} />
